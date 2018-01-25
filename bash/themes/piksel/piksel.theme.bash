7b016cac33286882764136cfbc5102b227535486
Nils Måsén <nils@piksel.se>
2018-01-25 20:01:09 +0100

    Fix bash prompt bug

--- bash/themes/piksel/piksel.theme.bash
+++ bash/themes/piksel/piksel.theme.bash
@@ -2,8 +2,8 @@
 
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-SCM_THEME_PROMPT_PREFIX=" |"
-SCM_THEME_PROMPT_SUFFIX="${green}|"
+SCM_THEME_PROMPT_PREFIX=" ["
+SCM_THEME_PROMPT_SUFFIX="${green}]"
 
 GIT_THEME_PROMPT_DIRTY=" ${red}✗"
 GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
@@ -13,15 +13,14 @@ GIT_THEME_PROMPT_SUFFIX="${yellow}]"
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
-
 function prompt_command() {
     local EXIT="$?"
     if [ "$EXIT" == 0 ]; then
-        res_prompt="\[$green\]✔"
+        res_prompt="$green✔"
     else
-        res_prompt="\[$red\]╳"
+        res_prompt="$red╳"
     fi
-    PS1="\n\[${cyan}\]\u\[$white\] @ \[$cyan\]\h \[$white\]\w \[$green\]$(scm_prompt_info)\n$res_prompt\[$reset_color\] ➡ "
+    PS1="\n\[$cyan\]\u\[$white\] @ \[$cyan\]\h \[$white\]\w\[$green\]$(scm_prompt_info)\n$res_prompt $reset_color➡ "
 }
 
 THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"${white}"}
